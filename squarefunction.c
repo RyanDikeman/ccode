05ac33826fe0f1b11670e1889d2a41cd6c9b2d3a
Ryan4245 <31550030+RyanDikeman@users.noreply.github.com>
2018-01-17 13:20:30 -0800

    Create squarefunction.c

---
+++ squarefunction.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int square ( int v );
+
+int main( )
+{
+        int x, n ;
+        printf ( "\nEnter some number for finding square \n");
+        scanf ( "%d", &n) ;
+        x = square ( n ) ;
+        printf ( "\nSquare of the given number %d id %d",n,x );
+        printf("\n\n");
+        return 0;
+}
+
+int square ( int v )  // function definition
+{
+        int p ;
+        p = v * v ;
+        return ( p ) ;
